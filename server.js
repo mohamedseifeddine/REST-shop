4c12b8840ba1099140fab1c083c526364c4bfc1c
hamza <hamzamohamedseifeddine@gmail.com>
2019-03-24 21:29:24 +0100

    creating rest api with node

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http")
+const app =require("./app")
+const port = process.env.PORT || 3000
+
+
+const server = http.createServer(app)
+
+server.listen(port,err=>{
+if(err){
+  console.log("server erreur")
+}
+else{
+  console.log(`server running on ${port}`)
+}
+
+})
